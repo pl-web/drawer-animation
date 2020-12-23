704d165743abee066ff7096974e30cd34d16080c
it-reader <hnature@126.com>
2020-12-23 22:54:48 +0800

    animation

---
+++ src/components/Drawer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="drawer">
+    <div class="not-collapse-content" @click="changeActive">未折叠内容</div>
+    <div :class="active?'collapse-content active':'collapse-content'">折叠内容</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: false
+    }
+  },
+  methods: {
+    changeActive() {
+      this.active = !this.active;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#drawer {
+  width: 500px;
+  height: auto;
+  border: 1px solid #ccc;
+  .not-collapse-content {
+    font-size: 100px;
+  }
+  .collapse-content {
+    font-size: 100px;
+    transform: scaleY(0);    
+    transform-origin: top;
+    transition: transform 0.26s ease;
+    height: 0px;
+    &.active {
+      transform: scaleY(1);
+      height: auto;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
+    <Drawer></Drawer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Drawer from '@/components/Drawer.vue'
 
 export default {
   name: 'home',
   components: {
-    
+    Drawer
   }
 }
 </script>
